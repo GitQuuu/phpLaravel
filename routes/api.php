4c91e1444d4114e3ef8acf2f403d27e9431e8b41
HomeQu <Quanvle@live.dk>
2022-06-07 11:30:19 +0200

    add routes to the api.php file

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,21 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('meeting','MeetingController',[
+    'except' => ['edit','create']
+]);
+
+Route::resource('meeting/registration','RegistrationController',[
+    'only' => ['store','destroy']
+]);
+
+Route::post('user',[
+    'uses' => ['AuthController@store']
+]);
+
+Route::post('user/signin',[
+    'uses' => ['AuthController@signin']
+]);
+
+
